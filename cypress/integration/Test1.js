ff2392b7c9505bd853f5af28d2474df9401b2918
dvoznyi <dvoznyi17@gmail.com>
2021-07-07 09:14:21 +0300

    test

---
+++ cypress/integration/Test1.js
@@ -0,0 +1,9 @@
+describe('dvTestSuite', () => {
+    it('VerifyTitleOfThePage', () => {
+        cy.visit('http://localhost:4200/pages/layout/stepper');
+        cy.get('.ng-star-inserted').contains ('Step content #1');
+        cy.get("button[type='submit'][aria-disabled='false']").contains('next').click();
+        cy.wrap('.label-index').should('have','"ng-reflect-ng-if": "true"');
+    })
+
+})
