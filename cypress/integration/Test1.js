20a56b3b9a353d3f5a5fbbfc8e2cf40af5bde4a8
dvoznyi <dvoznyi17@gmail.com>
2021-07-12 13:19:26 +0300

    Test123

--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -3,7 +3,14 @@ describe('dvTestSuite', () => {
         cy.visit('http://localhost:4200/pages/layout/stepper');
         cy.get('.ng-star-inserted').contains ('Step content #1');
         cy.get("button[type='submit'][aria-disabled='false']").contains('next').click();
-        cy.wrap('.label-index').should('have','"ng-reflect-ng-if": "true"')
-    })
+        cy.get('[class="step ng-star-inserted completed"]').contains('First step');
+        cy.get('.ng-star-inserted').contains ('Step content #2');
+        cy.get("button[type='submit'][aria-disabled='false']").contains('next').click();
+        cy.get('[class="step ng-star-inserted completed"]').contains('Second step');
+        cy.get('.ng-star-inserted').contains ('Step content #3');
+        cy.get("button[type='submit'][aria-disabled='false']").contains('next').click();
+        cy.get('[class="step ng-star-inserted completed"]').contains('Third step');
+        cy.get('.ng-star-inserted').contains ('Step content #4');
 
+    })
 })
