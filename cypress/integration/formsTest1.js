9368cbb33788330fcf93f2b0316e2b9916c015ca
dvoznyi <dvoznyi17@gmail.com>
2021-07-26 01:06:52 +0300

    gjfghdfgjf

--- cypress/integration/formsTest1.js
+++ cypress/integration/formsTest1.js
@@ -2,12 +2,8 @@ import formsPage from '../pages/formsPage'
 import layoutsPage  from '../pages/layoutsPage';
 it.only('Test1', ()  => {
 formsPage.visit();
-//cy.get("input[placeholder='Jane Doe']").type('Vasya')
 layoutsPage.inlineFormName()
-//cy.get("input[placeholder='Email'][type='text'").type('Petrovich@gmail.com')
 layoutsPage.inLineFormEmail()
-//cy.get('.text').contains ('Remember me').click()
 layoutsPage.rememberMeButton()
-//cy.get("button[ng-reflect-status='primary'][type='submit']").click({ multiple: true })
 layoutsPage.submitButton1()
 })
